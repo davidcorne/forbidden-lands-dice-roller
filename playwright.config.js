19ae0bceeeda1ee024ede4c926e3e76e62545837
David Corne <davidcorne@gmail.com>
2026-05-31 22:26:51 +0100

    Add a favicon

--- playwright.config.js
+++ playwright.config.js
@@ -2,6 +2,7 @@ import { defineConfig, devices } from '@playwright/test';
 
 export default defineConfig({
 	testDir: './e2e',
+	timeout: 5000,
 	webServer: {
 		command: 'npm run build && npm run preview -- --port 4173',
 		port: 4173,
@@ -9,8 +10,16 @@ export default defineConfig({
 	},
 	projects: [
 		{
-			name: 'chromium',
+			name: 'Chromium',
 			use: { ...devices['Desktop Chrome'] }
+		},
+		{
+			name: 'Mobile Chrome',
+			use: { ...devices['Pixel 7'] }
+		},
+		{
+			name: 'Microsoft Edge',
+			use: { ...devices['Desktop Edge'], channel: 'msedge' }
 		}
 	]
 });
